6c868ff05397bc0fa1e41b9d8df8efa76f0c938e
RAJ-Git18 <simkhadaraj98@gmail.com>
2025-04-21 19:14:12 +0545

    register user

---
+++ backend/gadget_ecommerce/accounts/serializers.py
@@ -0,0 +1,19 @@
+from .models import CustomUserModel
+from rest_framework import serializers
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUserModel
+        fields = ["user_id", "username", "first_name", "last_name", "email", "password"]
+
+        def validate(self, data):
+            db_username = CustomUserModel.objects.filter(username=data["username"]).exists()
+            db_email = CustomUserModel.objects.filter(email=data["email"]).exists()
+            if (db_username):
+                raise serializers.ValidationError({"username":"Username already exists"})
+            if (db_email):
+                raise serializers.ValidationError({"email":"Email already exists"})
+            
+
+            return data
---
+++ backend/gadget_ecommerce/accounts/views.py
@@ -0,0 +1,26 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import CustomUserModel
+from .serializers import RegisterSerializer
+
+
+class RegisterView(APIView):
+    def get(self, request):
+        queryset = CustomUserModel.objects.all()
+        serializer = RegisterSerializer(queryset, many= True)
+        return Response({"user_details": serializer.data})
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            CustomUserModel.objects.create_user(
+                username=request.data.get("username"),
+                first_name=request.data.get("firstname"),
+                last_name=request.data.get("lastname"),
+                email=request.data.get("email"),
+                password=request.data.get("password"),
+            )
+
+            return Response({"message": "Login successfull"})
+
+        return Response(serializer.errors)
